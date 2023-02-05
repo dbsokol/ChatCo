b6364e78b9c2d27ed2f66342b8a8628077fc3267
dbsokol <github@danielbsokol.engineer>
2023-02-05 12:34:41 -0800

    added typing socket

--- chatting/consumers.py
+++ chatting/consumers.py
@@ -35,28 +35,58 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # receive messages from websocket:
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        sender = text_data_json['sender']
-        m = await self.create_chat(message, sender)
 
-        # send message to room group:
-        await self.channel_layer.group_send(
-            self.group_name, {
-                'type': 'chat_message', 
-                'content': m.content,
-                'sender': m.sender.name,
-                'timestamp': m.timestamp.isoformat(),
-            }
-        )
+        # is typing messages:
+        if text_data_json['type'] == 'typing_status':
+
+            # send message to room group:
+            await self.channel_layer.group_send(
+                self.group_name, {
+                    'type': text_data_json['type'], 
+                    'is_typing': text_data_json['isTyping'],
+                    'sender': text_data_json['sender'],
+                }
+            )
+
+        # message content messages:
+        elif text_data_json['type'] == 'message_content':
+
+            # create a new message instance:
+            message = text_data_json['message']
+            sender = text_data_json['sender']
+            m = await self.create_chat(message, sender)
+
+            # send message to room group:
+            await self.channel_layer.group_send(
+                self.group_name, {
+                    'type': text_data_json['type'], 
+                    'content': m.content,
+                    'sender': m.sender.name,
+                    'timestamp': m.timestamp.isoformat(),
+                }
+            )
+
+    async def typing_status(self, event):
+        print(event)
+
+        # send message to websocket:
+        await self.send(text_data=json.dumps({
+            'type': 'typing_status',
+            'isTyping': event['is_typing'],
+            'sender': event['sender'],
+        }))
 
     # receive messages from room group:
-    async def chat_message(self, event):
+    async def message_content(self, event):
+        print(event)
+
         content = event['content']
         sender = event['sender']
         timestamp = event['timestamp']
-        print(event)
+        
         # send message to websocket:
         await self.send(text_data=json.dumps({
+            'type': 'message_content',
             'content': content,
             'sender': sender,
             'timestamp': timestamp,
